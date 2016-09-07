7064100067ebd1026d49c2943d0c87fc09cf35d5
Chris Georgoulis <cgeorgoulis@gmail.com>
2016-09-08 01:38:58 +0300

    updated issue where closing a dialog would throw an exception on IE because of a missing .focus() method

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -2649,7 +2649,7 @@ angular.module('ui.bootstrap.modal', [])
         //move focus to specified element if available, or else to body
         if (elementToReceiveFocus && elementToReceiveFocus.focus) {
           elementToReceiveFocus.focus();
-        } else {
+        } else if (body.focus) {
           body.focus();
         }
       }
